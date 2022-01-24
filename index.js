787d85dc158126f4b4b7a68c2a7ad9af55921d60
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2022-01-23 20:29:48 -0700

    Connect bing image search api

--- index.js
+++ index.js
@@ -1,4 +1,67 @@
+'use strict'
 require('dotenv').config()
+
+let https = require('https');
+
+let subscriptionKey = process.env.AZURE_SUB_KEY;
+
+let host = 'api.bing.microsoft.com';
+let path = '/v7.0/images/search';
+let term = 'Oregon\'s Wild Harvest - Turmeric, Organic Extract 1 fl oz';
+
+let response_handler = function (response) {
+  let body = '';
+
+  response.on('data', function (d){
+    body += d;
+  });
+
+  response.on('end', function(){
+    let imageResults = JSON.parse(body);
+
+    if (imageResults.value.length > 0) {
+      let firstImageResult = imageResults.value[0];
+      console.log(`Image result count: ${imageResults.value.length}`);
+      console.log(firstImageResult);
+      console.log(`First image insightsToken: ${firstImageResult.imageInsightsToken}`);
+      console.log(`First image thumbnail url: ${firstImageResult.thumbnailUrl}`);
+      console.log(`First image web search url: ${firstImageResult.webSearchUrl}`);
+      const image_url = firstImageResult.contentUrl;
+      console.log(image_url)
+    }
+    else {
+      console.log("Couldn't find image results!");
+    }
+  });
+
+  response.on('error', function (e) {
+    console.log('Error: ' + e.message);
+  });
+}
+
+let bing_image_search = function (search){
+  console.log('Searching images for: ' + term);
+  let request_params = {
+    method : 'GET',
+    hostname : host,
+    path : path + '?q=' + encodeURIComponent(search) + '&aspect=' + encodeURIComponent("square"),
+    headers : {
+      'Ocp-Apim-Subscription-Key' : subscriptionKey,
+    }
+  };
+
+  let req = https.request(request_params, response_handler);
+  req.end();
+}
+
+if (subscriptionKey.length === 32) {
+  bing_image_search(term);
+} else {
+  console.log('Invalid Bing Search API subscription key!');
+  console.log('Please paste yours into the source code.');
+}
+
+
 
 var fs = require('fs'),
   request = require('request')
@@ -17,7 +80,6 @@ var urlExists = require('url-exists')
 let mysql = require('mysql2')
 
 var gis = require('g-i-s')
-
 let connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
@@ -55,13 +117,16 @@ connection.query(
     let interval = setInterval(function () {
       if (i <= results.length) {
         const title = results[i]['Title']
+        const option = results[i]['Option1 Value'] === 'Default Title' ? '' : results[i]['Option1 Value']
         urlExists(results[i]["Image Src"], (err, exists) => {
 
           if(!exists){
             const opts = {
-              searchTerm: title,
+              searchTerm: title + " " + option,
               queryStringAddition: '&tbs=isz:lt,islt:vga,iar:s'
             }
+
+            console.log(opts)
 
             gis(opts, throttle(addResultToTable,500))
 
@@ -69,7 +134,7 @@ connection.query(
               if (error) {
                 console.log(error)
               } else {
-                const image = results[0]
+                const image = results[0]?.url
                 connection.query(
                   'UPDATE `all-shopify-products` SET `Image Src` = ? WHERE `Title` = ?',
                   [image, title],
@@ -87,9 +152,5 @@ connection.query(
         console.log("Finished");
       }
     },1000)
-
-    for (let i = 0; i < results.length; i++) {
-
-    }
   }
 )
